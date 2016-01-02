19f31d941a8f41051b1b9f1d86de31b177f39a06
Kevin Liang <siubombjai@gmail.com>
2016-01-01 23:05:59 -0800

    include controller in route

--- app/routes/route.js
+++ app/routes/route.js
@@ -1,13 +1,16 @@
-app.config(['$routeProvider',
-  function($routeProvider) {
+// angular help to setup route
+// https://github.com/angular/angular-phonecat/compare/step-6...step-7
+app.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
       when('/home1', {
-        templateUrl: 'public/partials/home1.html'
+        templateUrl: 'public/partials/home1.html',
+        controller: 'homeOneCtrl'
       }).
       when('/home2', {
-        templateUrl: 'public/partials/home2.html'
+        templateUrl: 'public/partials/home2.html',
+        controller: 'homeTwoCtrl'
       }).
       otherwise({
-        redirectTo: '/home'
+        redirectTo: '/'
       });
-  }]);
+}]);
